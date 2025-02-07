97812af718523fb86266cf1968d9614ff4be7581
Chun Hsiang Lee <105424632+ChunHisangLee@users.noreply.github.com>
2025-02-07 10:21:02 +0800

    Update do-lab.sh

--- lab1/part1/do-lab.sh
+++ lab1/part1/do-lab.sh
@@ -3,3 +3,15 @@
 # INCLUDE ALL COMMANDS NEEDED TO PERFORM THE LAB
 # This file will get called from capture_submission.sh
 
+set -euxo pipefail
+
+sudo ip link add name br0 type bridge 2>/dev/null || true
+
+sudo ip link set dev br0 up
+
+for intf in $(ip -o link show | grep 'clab-lab1-part1-host' | awk -F': ' '{print $2}'); do
+    sudo ip link set dev "$intf" master br0
+    sudo ip link set dev "$intf" up
+done
+
+echo "Bridge setup complete."
